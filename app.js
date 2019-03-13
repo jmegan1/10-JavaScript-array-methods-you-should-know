90df4f31b22efaad3313cfe4468f12e40b32366d
jeremiah egan <37743535+jmegan1@users.noreply.github.com>
2019-03-13 14:49:24 -0600

    added rest of 10 essential arrays
    hello moto

--- app.js
+++ app.js
@@ -52,3 +52,76 @@ const arr = [1, 2, 3, 4, 5, 6];
 const sum = arr.reduce((total, value) => total + value, 0);
 console.log(sum); // 21
 */
+
+
+/*
+//some()
+
+const arr = [1, 2, 3, 4, 5, 6];
+// at least one element is greater than 4?
+const largeNum = arr.some(num => num > 4);
+console.log(largeNum): // output: true
+
+// at least one element is less than or equal to 0?
+const small Num = arr.some(num => num <= 0);
+console.log(smallNum); // output: false . */
+
+/*
+every()
+
+  const arr = [1, 2, 3, 4, 5, 6];
+
+  // all elements are greater than 4
+  const greaterFour = arr.every(num => num > 4);
+  console.log(greaterFour); // output: false
+
+  // all elements are less than 10
+  const lessTen = arr.every(num => num < 10);
+  console.log(lessTen); // output: true . */
+
+  /*
+
+  sort()
+
+    const arr = [1, 2, 3, 4, 5, 6];
+  const alpha = ['e', 'a', 'c', 'u', 'y'];
+
+  // sort in descending order
+  descOrder = arr.sort((a, b) => a > b ? -1 : 1);
+  console.log(descOrder); // output: [6, 5, 4, 3, 2, 1]
+
+  // sort in ascending order
+  ascOrder = alpha.sort((a, b) => a > b ? 1 : -1);
+  console.log(ascOrder); // output: ['a', 'c', 'e', 'u', 'y'] . */
+
+  /*
+
+  Array.from()
+
+  const name = 'frugence';
+  const nameArray = Array.from(name);
+
+  console.log(name); // output: frugence
+  console.log(nameArray); // output: ['f', 'r', 'u', 'g', 'e', 'n', 'c', 'e']
+*/
+/*
+working with DOM
+
+  // I assume that you have created unorder list of items in our html file.
+
+  const lis = document.querySelectorAll('li');
+const lisArray = Array.from(document.querySelectorAll('li'));
+
+// is true array?
+console.log(Array.isArray(lis)); // output: false
+console.log(Array.isArray(lisArray));  // output: true
+*/
+
+/*
+Array.of()
+
+const nums = Array.of(1, 2, 3, 4, 5, 6);
+  console.log(nums); // output: [1, 2, 3, 4, 5, 6]
+
+  */
+
