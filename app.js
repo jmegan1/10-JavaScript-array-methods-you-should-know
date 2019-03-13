b3c33c62c17c9fca50105bf0968bc9aed2bdae11
jeremiah egan <37743535+jmegan1@users.noreply.github.com>
2019-03-13 14:38:12 -0600

    10 JavaScript array methods you should know

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
-var birthday = new Date(1985, 0, 15, 11, 15, 25);
-var birthday2 = new Date(1985, 0, 15, 11, 15, 25);
-
-//get the month of the date (0 - 11)
-console.log(birthday.getMonth());
-console.log(birthday.getFullYear());
-console.log(birthday.getDate());
-console.log(birthday.getDay());
-console.log(birthday.getHours());
-console.log(birthday.getTime());
-
-if (birthday.getTime() == birthday2.getTime()) {
-  console.log("birthdays are equal");
-} else {
-  console.log("birthdays are not equal");
+/*for (var i = 1; i < 101; i++) {
+  if (i % 15 == 0) document.write("fizzbuzz");
+  else if (i % 5 == 0) document.write("buzz");
+  else if (i % 3 == 0) document.write("fizz");
+  else document.write(i);
 }
+
+for (var i = 1; i < 101; i++){
+if (i % 15 == 0) console.log('fizzbuzz');
+else if( i % 5 == 0) console.log('fizz');
+else if( i % 3 == 0) console.log('buzz');
+else console.log(i);
+}*/
+/*
+forEach()
+const arr = [1, 2, 3, 4, 5, 6];
+arr.forEach(item => {
+  console.log(item); //document.write(item);
+}); */
+
+/*
+includes()
+const arr = [1, 2, 3, 4, 5, 6];
+arr.includes(2); // output true
+arr.includes(7); // ouput false */
+
+/*
+
+filter()
+
+//item(s) greater than 3
+const arr = [1, 2, 3, 4, 5, 6];
+const filtered = arr.filter(num => num > 3);
+console.log(filtered); //output: [4, 5, 6]
+console.log(arr); //output: [1, 2, 3, 4, 5, 6] . */
+
+/*
+map()
+
+const arr = [1, 2, 3, 4, 5, 6];
+// add one to every element
+const oneAdded = arr.map(num => num + 1);
+console.log(oneAdded);  // output [2, 3, 4, 5, 6, 7]
+
+console.log(arr); // ooutput [1, 2, 3, 4, 5, 6] . */
+
+/*
+//reduce();
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+const sum = arr.reduce((total, value) => total + value, 0);
+console.log(sum); // 21
+*/
